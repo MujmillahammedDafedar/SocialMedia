6e9d18ff55a2d41da324322314e8854473211cc5
MujmillahammedDafedar <mujmildafedaar@gmail.com>
2020-06-18 15:01:11 +0530

    design improved

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mychat/model/Auth.dart';
 import 'package:mychat/screens/homepage.dart';
-import 'package:mychat/model/globals.dart' as globals;
+import 'package:mychat/model/globals.dart';
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:mychat/screens/delay_animation.dart';
+
+import 'model/globals.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,40 +29,177 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
- final googleSignin gs = new googleSignin();
-   if(globals.isLoggedIn == true){
-         Navigator.push(
-         context,
-         MaterialPageRoute(
-         builder: (BuildContext context) => new Homepage()),);
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+  final googleSignin gs = new googleSignin();
+  final globals globalInstance = new globals();
+  final int delayedAmount = 500;
+  double _scale;
+  AnimationController _controller;
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 200,
+      ),
+      lowerBound: 0.0,
+      upperBound: 0.1,
+    )..addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
 
-   } else{
-        build(context);
- }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SafeArea(
-        child: Column(
-          children: <Widget>[
-           RaisedButton(
-             textColor: Colors.black,
-             child: Text('Sign in with google'),
-             onPressed: () {
-               setState(() {
-                 gs.signInWithGoogle().whenComplete(() =>
-                     Navigator.push(
-                         context,
-                         MaterialPageRoute(
-                             builder: (BuildContext context) => new Homepage()))
-                 );
-               });
-             },
-           ),
-            ],
-        ),
+    final color = Colors.white;
+    _scale = 1 - _controller.value;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                AvatarGlow(
+                  endRadius: 90,
+                  duration: Duration(seconds: 2),
+                  glowColor: Colors.white24,
+                  repeat: true,
+                  repeatPauseDuration: Duration(seconds: 2),
+                  startDelay: Duration(seconds: 1),
+                  child: Material(
+                      elevation: 8.0,
+                      shape: CircleBorder(),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        child: FlutterLogo(
+                          size: 50.0,
+                        ),
+                        radius: 50.0,
+                      )),
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "Hi There",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35.0,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 1000,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "I'm Reflectly",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35.0,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 2000,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "Your New Personal",
+                    style: TextStyle(fontSize: 20.0, color: color),
+                  ),
+                  delay: delayedAmount + 3000,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "Journaling  companion",
+                    style: TextStyle(fontSize: 20.0, color: color),
+                  ),
+                  delay: delayedAmount + 3000,
+                ),
+                SizedBox(
+                  height: 100.0,
+                ),
+                DelayedAnimation(
+                  child: GestureDetector(
+                    onTapDown: _onTapDown,
+                    onTapUp: _onTapUp,
+                    child: Transform.scale(
+                      scale: _scale,
+                      child: _animatedButtonUI,
+                    ),
+                  ),
+                  delay: delayedAmount + 4000,
+                ),
+                SizedBox(height: 20.0,),
+//                DelayedAnimation(
+//                  child: Text(
+//                    "I Already have An Account".toUpperCase(),
+//                    style: TextStyle(
+//                        fontSize: 20.0,
+//                        fontWeight: FontWeight.bold,
+//                        color: color),
+//                  ),
+//                  delay: delayedAmount + 5000,
+//                ),
+              ],
+            ),
+          )
+        //  Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: <Widget>[
+        //     Text('Tap on the Below Button',style: TextStyle(color: Colors.grey[400],fontSize: 20.0),),
+        //     SizedBox(
+        //       height: 20.0,
+        //     ),
+        //      Center(
+
+        //   ),
+        //   ],
+
+        // ),
       ),
     );
   }
+
+  Widget get _animatedButtonUI => Container(
+    height: 60,
+    width: 270,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(100.0),
+      color: Colors.white,
+    ),
+    child: Center(
+      child: FlatButton(
+        onPressed: (){
+          setState(() {
+            gs.signInWithGoogle().whenComplete(() =>
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => new Homepage()))
+            );
+          });
+        },
+        child: Text(
+          'Login With Google',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.teal,
+          ),
+
+        ),
+      ),
+    ),
+  );
+
+  void _onTapDown(TapDownDetails details) {
+    _controller.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    _controller.reverse();
+  }
+
+
 }
